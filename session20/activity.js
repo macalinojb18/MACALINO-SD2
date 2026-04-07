67c2d18ef6eedb274c778cef365edfc079566c7d
macalinojb18 <jbmmacalino.student@ua.edu.ph>
2026-04-07 19:27:35 +0800

    addsession20

---
+++ session20/activity.js
@@ -0,0 +1,25 @@
+let friends =  [];
+
+function showFriends(){
+    if(friends.length <= 0){
+        console.log("you don't have any friends yet.");
+    }else{
+        console.log(friends);
+    }
+
+}
+
+function addFriend(name){
+    let checkFriend = friends.includes(name.toUpperCase());
+    
+    if(checkFriend == true){
+        console.log(${name.toUpperCase()} is already on your friend list.)
+
+    }else{
+        friends.push(name.toUpperCase());
+        console.log(`You added ${name.toUpperCase``)
+    
+
+
+
+// Remove specific friend -> using their name
---
+++ session20/index.js
@@ -0,0 +1,10 @@
+console.log("hello World");
+
+//[SECTION] Array 
+//an array is also a storage for multiple elements/values
+// The best practice is values/elements stored in an array should be of the same data type.
+
+let pokemon = ["pikachu", "charmander", "squirtle"];
+console.log(pokemon);
+
+console.log(pokemon);
